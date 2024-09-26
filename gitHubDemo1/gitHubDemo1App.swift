e2806ccb04f9fb884109e68f28b154f49dc2118a
ohoud-habib <ohoudhabib@gmail.com>
2024-09-26 11:30:25 +0300

    Initial Commit

---
+++ gitHubDemo1/gitHubDemo1App.swift
@@ -0,0 +1,17 @@
+//
+//  gitHubDemo1App.swift
+//  gitHubDemo1
+//
+//  Created by ohoud on 23/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct gitHubDemo1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
